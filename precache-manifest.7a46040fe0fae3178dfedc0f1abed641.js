0c0a6091936be3b958d5d3f126215f3d2fdc7cef
Giacomo Debidda <jackdebidda@gmail.com>
2019-08-17 16:48:11 +0200

    Updates

---
+++ precache-manifest.7a46040fe0fae3178dfedc0f1abed641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904828b164e5db18246502497b3a0cde",
+    "url": "/sketchboard/index.html"
+  },
+  {
+    "revision": "3a092fedb4985b010458",
+    "url": "/sketchboard/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "7259535510d80f2491b9",
+    "url": "/sketchboard/static/css/main.3e9913e1.chunk.css"
+  },
+  {
+    "revision": "3a092fedb4985b010458",
+    "url": "/sketchboard/static/js/2.3a092fed.chunk.js"
+  },
+  {
+    "revision": "7259535510d80f2491b9",
+    "url": "/sketchboard/static/js/main.72595355.chunk.js"
+  },
+  {
+    "revision": "dbfed3e7c5da3e80524d",
+    "url": "/sketchboard/static/js/runtime~main.dbfed3e7.js"
+  }
+]);
