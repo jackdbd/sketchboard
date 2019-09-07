9aefde7f3136c098a37a4fbd5c55c2f5e3ebc875
Giacomo Debidda <jackdebidda@gmail.com>
2019-09-07 19:41:18 +0200

    Add SidebarLayout component and refactor CSS

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,61 @@
+import clsx from 'clsx';
 import React from 'react';
 
 import { InteractionsLogger } from '../InteractionsLogger';
 import { Main } from '../Main';
 import { Sidebar } from '../Sidebar';
+import { SidebarLayout } from '../SidebarLayout';
 
-import styles from './styles.module.css';
+console.log(Main, InteractionsLogger);
 
 export const App: React.FC<{}> = () => {
   return (
-    <div className={styles.app}>
-      <Main />
-      <Sidebar label={'Choose a shape to draw:'} />
-      <InteractionsLogger />
-    </div>
+    <SidebarLayout side={'right'}>
+      <div className={clsx('font-size:biggish')}>
+        Duis sagittis, est sit amet gravida tristique, purus lectus venenatis
+        urna, id #FFFFFF risus ut nunc. Nulla vel magna sit amet dui lobortis
+        commodo vitae vel nulla sit amet ante hendrerit tempus. Aliquam
+        tincidunt velit sit amet ante hendrerit tempus. Nulla lobortis tempus
+        commodo. Sed dapibus, lectus sit amet adipiscing egestas, mauris est
+        viverra nibh, iaculis pretium sem orci aliquet mauris. Donec tempus
+        tempus tellus, ac lacinia turpis mattis ac. Cras molestie risus a enim
+        Convallis; vitae luctus libero lacinia. Suspendisse potenti cras
+        molestie, risus a enim convallis vitae luctus libero lacinia. Maecenas
+        sit amet tellus nec mi gravida posuere non pretium magna. Vestibulum
+        text-align : justify lacus. Nulla vel magna sit amet dui lobortis
+        commodo vitae vel nulla sit amet ante hendrerit tempus. Donec et nisi
+        dictum felis sollicitudin congue. Sed mauris arcu, aliquet ultrices
+        malesuada sed, pretium id CTRL + V massa. Aliquam nisl enim, tristique
+        tempus placerat at, posuere in lectus. Nulla lobortis tempus commodo.
+        Fusce ac sodales magna. Potenti et eros sed justo – commodo bibendum non
+        at nunc. Donec a congue leo. Cras molestie risus a enim ‘convallis
+        vitae’ luctus libero lacinia. Duis sagittis, est sit amet gravida
+        tristique, purus lectus venenatis urna, id molestie magna risus ut nunc.
+        Maecenas sit amet tellus – nec mi gravida posuere non pretium magna.
+        Nulla vel magna sit — amet dui lobortis commodo — vitae vel nulla. Nunc
+        iaculis risus vel orci ornare dignissim sed vitae nulla. Sed dapibus,
+        lectus sit amet adipiscing egestas, mauris est viverra nibh, iaculis
+        pretium sem orci aliquet mauris. Vestibulum sit amet ipsum lacus… Cras
+        molestie risus a enim ‘convallis vitae’ luctus libero lacinia. Fusce ac
+        sodales CSS magna. Nulla vel magna sit amet dui lobortis commodo vitae
+        vel nulla sit amet ante hendrerit tempus. Sed dapibus, lectus sit amet
+        adipiscing egestas, mauris est viverra nibh (iaculis pretium sem orci
+        aliquet mauris). Nulla lobortis tempus commodo. Aliquam nisl enim,
+        tristique tempus placerat at, posuere in lectus. Sed dapibus, lectus sit
+        amet adipiscing egestas, mauris est viverra nibh (iaculis pretium sem
+        orci aliquet mauris). Donec tempus tempus tellus, ac lacinia turpis
+        mattis ac! Nunc iaculis risus vel orci ornare dignissim sed vitae nulla.
+        Nulla auctor eleifend turpis consequat pharetra. Nulla vel magna sit
+        amet dui lobortis commodo vitae vel nulla. Suspendisse potenti. Maecenas
+        sit amet tellus nec mi gravida posuere non pretium magna. Nulla lobortis
+        tempus commodo. Nulla vel magna sit — amet dui lobortis commodo — vitae
+        vel nulla. Vestibulum sit amet ipsum lacus. Nulla auctor eleifend turpis
+        consequat pharetra. Maecenas sit amet tellus nec mi gravida posuere non
+        pretium magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse potenti cras molestie, risus a enim convallis vitae luctus
+        libero lacinia. Fusce ac sodales .generate() magna.
+      </div>
+      <Sidebar label={'some label'} />
+    </SidebarLayout>
   );
 };
---
+++ src/components/SidebarLayout/SidebarLayout.tsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+import React, { useEffect } from 'react';
+
+interface Props {
+  /**
+   * The sidebar and the non-sidebar element.
+   */
+  children: React.ReactElement[];
+
+  /**
+   * Make the adjacent elements adopt their natural height.
+   */
+  noStretch?: boolean;
+
+  /**
+   * The side where the sidebar is on.
+   */
+  side: 'left' | 'right';
+}
+
+export const SidebarLayout: React.FC<Props> = props => {
+  const { children, noStretch, side } = props;
+
+  useEffect(() => {
+    if (children.length !== 2) {
+      throw new Error('The Sidebar layout must have exactly 2 children');
+    }
+  }, [children, side]);
+
+  return (
+    <div className={clsx(`with-sidebar-on-the-${side}`)}>
+      <div className={clsx(noStretch && 'not-stretched-wrapper')}>
+        {children}
+      </div>
+    </div>
+  );
+};
