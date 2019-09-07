41077a2db3aad8d592c2f00e624a3579dc099dfd
Giacomo Debidda <jackdebidda@gmail.com>
2019-09-07 17:47:39 +0200

    Remove service worker
    No service worker is used at the moment, so it doesn't make sense to
    leave it here.

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,11 @@ import * as Sentry from '@sentry/browser';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './css/index.css';
-
 import { App } from './components/App';
-// import * as serviceWorker from "./serviceWorker";
 
+import './css/index.css';
 Sentry.init({
   dsn: 'https://94f09b0a51c74127b82e3fa4d47857c2@sentry.io/1547393',
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
