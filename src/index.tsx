d2b500922f32f655638ff94cde2c0d7dc7bc7aa1
Giacomo Debidda <jackdebidda@gmail.com>
2019-09-07 17:48:48 +0200

    Add documentation for Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,13 @@ import ReactDOM from 'react-dom';
 import { App } from './components/App';
 
 import './css/index.css';
+
+/**
+ * We could use the CDN hosted version of the Sentry JavaScript browser SDK, but
+ * Ad blockers would probably block Sentry from submitting crash reports.
+ * Instead, we add Sentry as a dependency (unfortunately, this means that it
+ * will end up in the final bundle).
+ */
 Sentry.init({
   dsn: 'https://94f09b0a51c74127b82e3fa4d47857c2@sentry.io/1547393',
 });
